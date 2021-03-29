aab2dfdfc573792a1e6673c2c12ac198da2bbdc6
chexovperm <chexovpert@gmail.com>
2021-03-29 23:02:18 +0300

    fix: fix audiogame

--- src/js/blocks/audioChallenge-block.js
+++ src/js/blocks/audioChallenge-block.js
@@ -25,23 +25,27 @@ export default (props) => {
   const [image, setImage] = useState(null);
 
   const quessHandler = (event) => {
-    if (event.target.textContent === baseWord.word) {
+    if (event.target.value === baseWord.id) {
       console.log("Correct");
-      setToggle(false);
-      setTimeout(newWords, 1200);
+      setCorrect(true);
     } else {
       console.log("wrong");
     }
   };
+  const nextpageHandler = () => {
+    setToggle(false);
+    setTimeout(newWords, 1200);
+  };
   const newWords = () => {
     setCorrect(false);
     setToggle(true);
+
     let updateData = data.slice().sort(() => Math.random() - 0.5);
-    //setData(updateData);
+
     setGuess([updateData[0]]);
     setBaseWord(updateData[0]);
     setUrl(`${baseUrl}${updateData[0].audio}`);
-    setImage(`${baseUrl}${updateData[0].image}`)
+    setImage(`${baseUrl}${updateData[0].image}`);
     const guesses = [updateData[0]];
     let baseDataUpdate = baseData
       .slice()
@@ -60,13 +64,8 @@ export default (props) => {
     newWords();
   }, [props.data]);
 
-  useEffect(() => {
-    //console.log(url);
-    changeSrc(`${url}`);
-  }, url);
-
   if (data.length > 0) {
-    console.log(image)
+    console.log(image);
     return (
       <CSSTransition
         in={toggle}
@@ -76,52 +75,89 @@ export default (props) => {
         }}
         mountOnEnter
         unmountOnExit
-        classNames="wb"
+        classNames="audChall-buttons"
+        onEntered={() => {
+          toggleAudio();
+          changeSrc(`${url}`);
+        }}
       >
         <div style={{ width: "100%" }}>
-          <CSSTransition
-            in={toggle}
-            timeout={{
-              enter: 15000,
-              exit: 1000,
-            }}
-            mountOnEnter
-            unmountOnExit
-            classNames="ap"
-          >
-            <div className="audioChallenge__base">
-              <div>
-              <div ><img src={image} className="audioChallenge__base-image"></img></div>
-              <div ><button  className="audioChallenge__base-button">smth</button><div><h2>{baseWord && baseWord.word}</h2></div></div>
-              </div>
-              <div>
-              <button
-                className={
-                  playing
-                    ? "audioChallenge__base-button play"
-                    : "audioChallenge__base-button"
-                }
-                style={{ margin: "0 auto" }}
-                onClick={toggleAudio}
-              >
-                {playing ? "Pause" : "Play"}
-              </button>
-              </div>
+          <div className="audioChallenge__base-word-container">
+            <div className="audioChallenge__base-button-container">
+              {!correct && (
+                <button
+                  className={
+                    playing
+                      ? "audioChallenge__base-button play"
+                      : "audioChallenge__base-button"
+                  }
+                  style={{ margin: "0 auto" }}
+                  onClick={toggleAudio}
+                >
+                  {playing ? "Pause" : "Play"}
+                </button>
+              )}
             </div>
-          </CSSTransition>
 
+            <CSSTransition
+              in={correct}
+              timeout={{
+                enter: 1000,
+                exit: 1000,
+              }}
+              mountOnEnter
+              unmountOnExit
+              classNames="ap-card"
+            >
+              <div className="audioChallenge__card-correct">
+                <div className="audioChallenge__card-correct-image">
+                  <img src={image} className="audioChallenge__base-image"></img>
+                </div>
+                <div className="audioChallenge__card-correct-bottom">
+                  <button
+                    onClick={toggleAudio}
+                    className={
+                      playing
+                        ? "audioChallenge__base-button play"
+                        : "audioChallenge__base-button"
+                    }
+                  >
+                    {playing ? "Pause" : "Play"}
+                  </button>
+                  <div className="audioChallenge__base-correct-word">
+                    <h2>{baseWord && baseWord.word}</h2>
+                  </div>
+                </div>
+              </div>
+            </CSSTransition>
+          </div>
           <div className="savanna__component">
             <div className="savanna__component-buttons">
               {guess &&
                 guess.map((word) => (
-                  <button className="header__link login" onClick={quessHandler}>
+                  <button
+                    value={word.id}
+                    disabled={correct}
+                    className={
+                      correct
+                        ? `audioChallenge__anwsers-button ${
+                            word.id === baseWord.id ? "true" : "wrong"
+                          }`
+                        : "audioChallenge__anwsers-button"
+                    }
+                    onClick={quessHandler}
+                  >
                     {word.wordTranslate}
                   </button>
                 ))}
             </div>
 
-            <button className="header__link login" style={{ margin: "0 auto" }}>
-              next
+            <button
+              onClick={nextpageHandler}
+              className="header__link login"
+              style={{ margin: "0 auto" }}
+            >
+              {correct ? "следующее слово" : "я не знаю"}
             </button>
           </div>
         </div>
