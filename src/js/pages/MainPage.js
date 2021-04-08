3fbae61bfaa3fda1b9e5e4487f7ed125e27ff28a
Chexovpert <chexovpert@gmail.com>
2021-04-08 16:20:51 +0300

    fix: mainpage fixes

--- src/js/pages/MainPage.js
+++ src/js/pages/MainPage.js
@@ -119,22 +119,41 @@ export default () => {
           </div>
         </div>
       </section>
-      <div className="mainPage_startScreen">
-        <div className="mainPage_startScreen_textContent">
-          <h1>Учите новые слова каждый день с RSlang</h1>
-          <p>
-            Уникальное приложение для изучения английского. Увлекательные игры
-            для тренировки слов и метод интервального повторения для запоминания
-            слов навсегда.
-          </p>
-          <Link to="/classbook" className="header__link login">
-            Начать обучение
-          </Link>
+      <section className="mainPage__team">
+        <h2>О команде</h2>
+        <div
+          className="mainPage__team__wrapper"
+          // style={{ backgroundImage: `url(${laptop})` }}
+        >
+          <div className="mainPage__team__card">
+          <img
+                    className="mainPage__team__card-image"
+                    src={startScreenImage}
+                  />
+                  <div  className="mainPage__team__card_description">
+                    <h3>chexovert</h3>
+                    <h5>Takoyto</h5>
+                    <p>что-то сделалfddfsdfdfshdf fdshksdfkjfdhsdfjkdfh kdfjsjhsdfjk sdfhsdfkhsdfjkjh sdfjksvhudfghj kgrhkdfhjksdf hfhdhksdfagukef ruyier i fdjhgdjk fdhgfdjkghk fdjhdfgkgfhgkdf fdgjhdgfgkd u</p>
+                  </div>
+                  <a href="/github">link</a>
+                  
+          </div>
+          <div className="mainPage__team__card">
+          <img
+                    className="mainPage__team__card-image"
+                    src={startScreenImage}
+                  />
+                  <div  className="mainPage__team__card_description">
+                    <h3>chexovert</h3>
+                    <h5>Takoyto</h5>
+                    <p>что-то сделалfddfsdfdfshdf fdshksdfkjfdhsdfjkdfh kdfjsjhsdfjk sdfhsdfkhsdfjkjh sdfjksvhudfghj kgrhkdfhjksdf hfhdhksdfagukef ruyier i fdjhgdjk fdhgfdjkghk fdjhdfgkgfhgkdf fdgjhdgfgkd u</p>
+                  </div>
+                  <a href="/github">link</a>
+                  
+          </div>
+          
         </div>
-        <div>
-          <img className="mainPage_startScreen_image" src={startScreenImage} />
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
