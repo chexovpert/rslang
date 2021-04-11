c6ce9d993a6e7e31c8c1830434f4bb9f1908f6ad
vepsar <vepsarvz@gmail.com>
2021-04-11 23:11:09 +0300

    fix: fix bugs 5

--- src/js/pages/textbook.js
+++ src/js/pages/textbook.js
@@ -1,39 +1,53 @@
+import { LinearProgress } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/blocks/textbook.scss";
 
 export default function TextBook() {
+  const [value, setValue] = useState(10);
+  const [pers, setPers] = useState(15);
+  useEffect(() => {
+    if ("correctwordid" in localStorage) {
+      setValue(JSON.parse(localStorage.getItem("correctwordid")));
+    }
+    setPers(value.length / 36);
+  }, []);
   return (
     <div className="textbook">
       <div className="textbook__logo">
-        <p>{`RSLang`}</p>
-        <p>{`Изучаем язык с удовольствием`}</p>
+        <h1>{`Учебник RSLang`}</h1>
+        <p>{`Добро пожаловать на страницу учебника`}</p>
+        <p>{`Здесь вы можете изучать новые слова или же повторить уже изученные или сложные слова`}</p>
+      </div>
+
+      <div className="textbook__learn">
+        <Link to="/testing">
+          <div className="textbook__cell">
+            <h3 style={{ color: "rgb(165, 0, 0)" }}>MOST IMPOTANT</h3>
+            <p>{"Изучить слова на сегодня"}</p>
+          </div>
+        </Link>
+        <Link to={"/wordlist/1/1"}>
+          <div className="textbook__cell">
+            <h4 style={{ color: "green", textDecoration: "underline" }}>К сведению</h4>
+            <p>{"Посмотреть весь список слов"}</p>
+          </div>
+        </Link>
+        <Link to={"/testingdif"}>
+          <div className="textbook__cell">
+            <h4 style={{ color: "blue", textDecoration: "dash ed" }}>НАПОМИНАНИЕ!!!</h4>
+            <p>{"Повторить сложные слова"}</p>
+          </div>
+        </Link>
       </div>
       <div className="textbook__statistic">
-        <p>{`Здесь вы можете изучать новые слова или же повторить уже изученные или сложные слова`}</p>
-        {"Выучено слов за сегодня"}
-        <p>{"Тут будет статистика(добавить прогресс бар)"}</p>
-      </div>
-      <div className="textbook__learn">
-        <div className="textbook__cell">
-          <p>{"Тут сделать начать дневное обучение"}</p>
-          <Link to="/testing">
-            <button>{`Начать обучение`}</button>
-          </Link>
-        </div>
-        <div className="textbook__cell">
-          <p>{"Посмотреть весь список слов"}</p>
-          <Link to={"/wordlist/1/1"}>
-            <button>{`Посмотреть слова`}</button>
-          </Link>
-        </div>
-        <div className="textbook__cell">
-          <p>{"Повторить сложные слова"}</p>
-          <Link to={"/testingdif"}>
-            <button>{`Повториь сложные слова`}</button>
-          </Link>
+        <h2>Выучено всего слов</h2>
+        <p>{`Изучено  ${value.length} из 3600 слов`}</p>
+        <div className="textbook__linear">
+          <LinearProgress variant="determinate" value={pers} />
         </div>
       </div>
-      <div>Здесь можно разместить чтото еще</div>
+      {/* <div>Здесь можно разместить чтото еще</div> */}
     </div>
   );
 }
